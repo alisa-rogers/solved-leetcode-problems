bd714cedfef6f2a8b9884a85d636c6c9747a010f
alisa-rogers <59536586+alisa-rogers@users.noreply.github.com>
2020-04-18 15:50:02 +0300

    Update valid palindrome.js

--- valid palindrome.js
+++ valid palindrome.js
@@ -6,6 +6,10 @@ var isPalindrome = function(s) {
     let newS = s.replace(/[.,\/#!$%\^&\*;:{}=\-_`?~() ]/g,"");
     let finalS = newS.toLowerCase();
     
+    if (s.length === 0 || s.length === 1) {
+        return true;
+    };
+    
     let left = 0, right = finalS.length - 1;
     
      while (left < right) {
