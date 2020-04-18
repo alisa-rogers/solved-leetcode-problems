54c1224c56d2f2d76b3f0cac8849c702a3d638b3
alisa-rogers <59536586+alisa-rogers@users.noreply.github.com>
2020-04-18 16:57:15 +0300

    Create longest common prefix.js

---
+++ longest common prefix.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    
+    if (strs.length === 0) {
+        return "";
+    };
+    if (strs.length === 1) {
+        return strs[0];
+    };
+    
+let order = strs.sort((a,b)=> a.length - b.length);
+let short = order[0];
+        for (let w = 1, l = 0; w < order.length, l < short.length; w++) { 
+            let substr;
+            
+            if (order[0][l] !== order[w][l]) {
+                return "";   
+            };
+            
+            while (order[0][l] === order[w][l]) {   /*letter change*/ 
+                l++;
+                let substr = order[0].substring (0, [l+1]);
+                return substr;
+            };
+        };
+};
