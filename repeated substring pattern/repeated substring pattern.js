1c224104314edb7710cfdf22166e15b57ad088b2
alisa-rogers <59536586+alisa-rogers@users.noreply.github.com>
2020-05-30 00:25:16 +0300

    Create repeated substring pattern.js

---
+++ repeated substring pattern/repeated substring pattern.js
@@ -0,0 +1,25 @@
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var repeatedSubstringPattern = function(s) {
+    if (s.length === 1) {
+    return false;
+    };
+    let length = s.length/2;
+    let result;
+    for (let i = 1; i <= length; i++) {
+        let substring = s.substring(0, i);
+        let array = s.split(substring);
+        let filtered = array.filter(function(value, index, arr) {return value !== ""});
+        if (filtered.length === 0) {
+            result = true;
+            break;
+        }
+        else {
+            result = false;
+        }
+    };
+     return result;
+};
