3efd682a4c048c93812215430ba0dc5d61f9e7f6
alisa-rogers <59536586+alisa-rogers@users.noreply.github.com>
2020-06-17 17:36:39 +0300

    Update last stone weight.js

--- last stone weight/last stone weight.js
+++ last stone weight/last stone weight.js
@@ -4,21 +4,18 @@
  */
 var lastStoneWeight = function(stones) {
     stones.sort(function(a, b){return b-a});
-    let newstone;
     while (stones.length > 1)
     if (stones[0] === stones[1]) {
         stones.splice (0, 2);
     }
     else if (stones[0] > stones[1]) {
-        newstone = stones[0] - stones[1];
-        stones.splice (0, 2);
-        stones.push (newstone);
+        stones[0] -= stones[1];
+        stones.splice (1, 1);
         stones.sort(function(a, b){return b-a}); 
     }
     else if (stones[0] < stones[1]){
-        newstone = stones[1] - stones[0];
-        stones.splice (0, 2);
-        stones.push (newstone);
+        stones[1] -= stones[0];
+        stones.splice (0, 1);
         stones.sort(function(a, b){return b-a});
     };
     if (stones.length === 0) {
