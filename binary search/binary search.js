36f682c9404c5bfe601cb3e07a8976bc225bfb39
alisa-rogers <59536586+alisa-rogers@users.noreply.github.com>
2020-06-19 16:59:15 +0300

    Create binary search.js

---
+++ binary search/binary search.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let startIndex = 0;
+    let endIndex = nums.length - 1;
+    while (startIndex <= endIndex) {
+        let middleIndex = Math.floor ((startIndex + endIndex) / 2);
+        if (target === nums[middleIndex]) {
+            return middleIndex;
+        }
+        else if (target < nums[middleIndex]) {
+            endIndex = middleIndex - 1;
+        }
+        else if (target > nums[middleIndex]) {
+            startIndex = middleIndex + 1;
+        };
+    };  
+     if (startIndex > endIndex) {
+        return -1;
+    };
+};
